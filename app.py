63072f65006545d660de58c7f34f59a50ac36a11
Teoh TeikToe <ttt@mensa.org.sg>
2023-03-13 18:29:49 +0800

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 def index():
     if request.method == "POST":
         t = request.form.get("txt")
-        openai.api_key = "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"
+        openai.api_key = "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"
         responce = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": t}])
         r = responce["choices"][0]["message"]["content"]
         return(render_template("index.html",result=r))
