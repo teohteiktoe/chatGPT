b85963851e88f8bc979a9b5344d445ba733a4b8c
Teoh TeikToe <ttt@mensa.org.sg>
2023-03-13 12:22:05 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from flask import Flask,render_template,request
+import openai
+
+app = Flask(__name__)
+
+@app.route("/",methods=["GET","POST"])
+def index():
+    if request.method == "POST":
+        t = request.form.get("txt")
+        openai.api_key = "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"
+        responce = openai.ChatCompletion.create(model="gpt-3.5-turbo",
+            messages=[{"role": "user", "content": t}])
+        r = responce["choices"][0]["message"]["content"]
+        return(render_template("index.html",result=r))
+    else:
+        return(render_template("index.html",result="waiting"))
+
+if __name__ == "__main__":
+    app.run()
+
+
+# In[ ]:
+
+
+
+
