948df32518218bbabbc787a7eefc44507e167152
Teoh TeikToe <ttt@mensa.org.sg>
2023-03-28 11:11:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from flask import Flask,render_template,request
 import openai
 import os
 
-openai.api_key = os.getenv('OPENAI_API_KEY’)
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 app = Flask(__name__)
 
