8435817c5f4f449fd5f0227cb9c4b9ddc98acdca
Teoh TeikToe <ttt@mensa.org.sg>
2024-02-17 09:14:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,27 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 from flask import Flask,render_template,request
-import openai
-import os
-openai.api_key = os.getenv("OPENAI_API_KEY")
 
+from openai import OpenAI
+
+client = OpenAI(
+    api_key="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",
+)
 
 app = Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
 def index():
     if request.method == "POST":
-        t = request.form.get("txt")
-        responce = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": t}])
-        r = responce["choices"][0]["message"]["content"]
-        return(render_template("index.html",result=r))
+        q = request.form.get("txt")
+        r = client.chat.completions.create(
+            model="gpt-3.5-turbo", 
+            messages=[{"role": "user", "content": q}],
+        )
+        return(render_template("index.html",result=r.choices[0].message.content))
     else:
         return(render_template("index.html",result="waiting"))
 
 if __name__ == "__main__":
     app.run()
-
-
-# In[ ]:
 
 
 
